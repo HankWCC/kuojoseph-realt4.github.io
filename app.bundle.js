f37daf29bc93d10475a71dbc1f7dc96eb0d73249
Joseph Kuo <joseph.kuo@realt4.com>
2022-03-01 01:09:12 +0800

    Updates

--- app.bundle.js
+++ app.bundle.js
@@ -1,7 +1,7 @@
 "use strict";
 (self["webpackChunkkuojoseph_realt4_github_io"] = self["webpackChunkkuojoseph_realt4_github_io"] || []).push([[143],{
 
-/***/ 226:
+/***/ 629:
 /***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
 
 
@@ -6683,6 +6683,11 @@ UnrealBloomPass.BlurDirectionY = new three_module/* Vector2 */.FM8( 0.0, 1.0 );
 
 
 
+;// CONCATENATED MODULE: ./node_modules/three/examples/jsm/libs/tween.module.min.js
+// tween.js 17.3.5 - https://github.com/tweenjs/tween.js
+var _Group=function(){this._tweens={},this._tweensAddedDuringUpdate={}};_Group.prototype={getAll:function(){return Object.keys(this._tweens).map(function(t){return this._tweens[t]}.bind(this))},removeAll:function(){this._tweens={}},add:function(t){this._tweens[t.getId()]=t,this._tweensAddedDuringUpdate[t.getId()]=t},remove:function(t){delete this._tweens[t.getId()],delete this._tweensAddedDuringUpdate[t.getId()]},update:function(t,n){var e=Object.keys(this._tweens);if(0===e.length)return!1;for(t=void 0!==t?t:TWEEN.now();0<e.length;){this._tweensAddedDuringUpdate={};for(var i=0;i<e.length;i++){var r=this._tweens[e[i]];r&&!1===r.update(t)&&(r._isPlaying=!1,n||delete this._tweens[e[i]])}e=Object.keys(this._tweensAddedDuringUpdate)}return!0}};var TWEEN=new _Group;TWEEN.Group=_Group,TWEEN._nextId=0,TWEEN.nextId=function(){return TWEEN._nextId++},"undefined"==typeof self&&"undefined"!=typeof process&&process.hrtime?TWEEN.now=function(){var t=process.hrtime();return 1e3*t[0]+t[1]/1e6}:"undefined"!=typeof self&&void 0!==self.performance&&void 0!==self.performance.now?TWEEN.now=self.performance.now.bind(self.performance):void 0!==Date.now?TWEEN.now=Date.now:TWEEN.now=function(){return(new Date).getTime()},TWEEN.Tween=function(t,n){this._object=t,this._valuesStart={},this._valuesEnd={},this._valuesStartRepeat={},this._duration=1e3,this._repeat=0,this._repeatDelayTime=void 0,this._yoyo=!1,this._isPlaying=!1,this._reversed=!1,this._delayTime=0,this._startTime=null,this._easingFunction=TWEEN.Easing.Linear.None,this._interpolationFunction=TWEEN.Interpolation.Linear,this._chainedTweens=[],this._onStartCallback=null,this._onStartCallbackFired=!1,this._onUpdateCallback=null,this._onCompleteCallback=null,this._onStopCallback=null,this._group=n||TWEEN,this._id=TWEEN.nextId()},TWEEN.Tween.prototype={getId:function(){return this._id},isPlaying:function(){return this._isPlaying},to:function(t,n){return this._valuesEnd=Object.create(t),void 0!==n&&(this._duration=n),this},duration:function(t){return this._duration=t,this},start:function(t){for(var n in this._group.add(this),this._isPlaying=!0,this._onStartCallbackFired=!1,this._startTime=void 0!==t?"string"==typeof t?TWEEN.now()+parseFloat(t):t:TWEEN.now(),this._startTime+=this._delayTime,this._valuesEnd){if(this._valuesEnd[n]instanceof Array){if(0===this._valuesEnd[n].length)continue;this._valuesEnd[n]=[this._object[n]].concat(this._valuesEnd[n])}void 0!==this._object[n]&&(this._valuesStart[n]=this._object[n],this._valuesStart[n]instanceof Array==!1&&(this._valuesStart[n]*=1),this._valuesStartRepeat[n]=this._valuesStart[n]||0)}return this},stop:function(){return this._isPlaying&&(this._group.remove(this),this._isPlaying=!1,null!==this._onStopCallback&&this._onStopCallback(this._object),this.stopChainedTweens()),this},end:function(){return this.update(1/0),this},stopChainedTweens:function(){for(var t=0,n=this._chainedTweens.length;t<n;t++)this._chainedTweens[t].stop()},group:function(t){return this._group=t,this},delay:function(t){return this._delayTime=t,this},repeat:function(t){return this._repeat=t,this},repeatDelay:function(t){return this._repeatDelayTime=t,this},yoyo:function(t){return this._yoyo=t,this},easing:function(t){return this._easingFunction=t,this},interpolation:function(t){return this._interpolationFunction=t,this},chain:function(){return this._chainedTweens=arguments,this},onStart:function(t){return this._onStartCallback=t,this},onUpdate:function(t){return this._onUpdateCallback=t,this},onComplete:function(t){return this._onCompleteCallback=t,this},onStop:function(t){return this._onStopCallback=t,this},update:function(t){var n,e,i;if(t<this._startTime)return!0;for(n in!1===this._onStartCallbackFired&&(null!==this._onStartCallback&&this._onStartCallback(this._object),this._onStartCallbackFired=!0),e=(t-this._startTime)/this._duration,e=0===this._duration||1<e?1:e,i=this._easingFunction(e),this._valuesEnd)if(void 0!==this._valuesStart[n]){var r=this._valuesStart[n]||0,a=this._valuesEnd[n];a instanceof Array?this._object[n]=this._interpolationFunction(a,i):("string"==typeof a&&(a="+"===a.charAt(0)||"-"===a.charAt(0)?r+parseFloat(a):parseFloat(a)),"number"==typeof a&&(this._object[n]=r+(a-r)*i))}if(null!==this._onUpdateCallback&&this._onUpdateCallback(this._object),1!==e)return!0;if(0<this._repeat){for(n in isFinite(this._repeat)&&this._repeat--,this._valuesStartRepeat){if("string"==typeof this._valuesEnd[n]&&(this._valuesStartRepeat[n]=this._valuesStartRepeat[n]+parseFloat(this._valuesEnd[n])),this._yoyo){var s=this._valuesStartRepeat[n];this._valuesStartRepeat[n]=this._valuesEnd[n],this._valuesEnd[n]=s}this._valuesStart[n]=this._valuesStartRepeat[n]}return this._yoyo&&(this._reversed=!this._reversed),void 0!==this._repeatDelayTime?this._startTime=t+this._repeatDelayTime:this._startTime=t+this._delayTime,!0}null!==this._onCompleteCallback&&this._onCompleteCallback(this._object);for(var o=0,u=this._chainedTweens.length;o<u;o++)this._chainedTweens[o].start(this._startTime+this._duration);return!1}},TWEEN.Easing={Linear:{None:function(t){return t}},Quadratic:{In:function(t){return t*t},Out:function(t){return t*(2-t)},InOut:function(t){return(t*=2)<1?.5*t*t:-.5*(--t*(t-2)-1)}},Cubic:{In:function(t){return t*t*t},Out:function(t){return--t*t*t+1},InOut:function(t){return(t*=2)<1?.5*t*t*t:.5*((t-=2)*t*t+2)}},Quartic:{In:function(t){return t*t*t*t},Out:function(t){return 1- --t*t*t*t},InOut:function(t){return(t*=2)<1?.5*t*t*t*t:-.5*((t-=2)*t*t*t-2)}},Quintic:{In:function(t){return t*t*t*t*t},Out:function(t){return--t*t*t*t*t+1},InOut:function(t){return(t*=2)<1?.5*t*t*t*t*t:.5*((t-=2)*t*t*t*t+2)}},Sinusoidal:{In:function(t){return 1-Math.cos(t*Math.PI/2)},Out:function(t){return Math.sin(t*Math.PI/2)},InOut:function(t){return.5*(1-Math.cos(Math.PI*t))}},Exponential:{In:function(t){return 0===t?0:Math.pow(1024,t-1)},Out:function(t){return 1===t?1:1-Math.pow(2,-10*t)},InOut:function(t){return 0===t?0:1===t?1:(t*=2)<1?.5*Math.pow(1024,t-1):.5*(2-Math.pow(2,-10*(t-1)))}},Circular:{In:function(t){return 1-Math.sqrt(1-t*t)},Out:function(t){return Math.sqrt(1- --t*t)},InOut:function(t){return(t*=2)<1?-.5*(Math.sqrt(1-t*t)-1):.5*(Math.sqrt(1-(t-=2)*t)+1)}},Elastic:{In:function(t){return 0===t?0:1===t?1:-Math.pow(2,10*(t-1))*Math.sin(5*(t-1.1)*Math.PI)},Out:function(t){return 0===t?0:1===t?1:Math.pow(2,-10*t)*Math.sin(5*(t-.1)*Math.PI)+1},InOut:function(t){return 0===t?0:1===t?1:(t*=2)<1?-.5*Math.pow(2,10*(t-1))*Math.sin(5*(t-1.1)*Math.PI):.5*Math.pow(2,-10*(t-1))*Math.sin(5*(t-1.1)*Math.PI)+1}},Back:{In:function(t){return t*t*(2.70158*t-1.70158)},Out:function(t){return--t*t*(2.70158*t+1.70158)+1},InOut:function(t){var n=2.5949095;return(t*=2)<1?t*t*((1+n)*t-n)*.5:.5*((t-=2)*t*((1+n)*t+n)+2)}},Bounce:{In:function(t){return 1-TWEEN.Easing.Bounce.Out(1-t)},Out:function(t){return t<1/2.75?7.5625*t*t:t<2/2.75?7.5625*(t-=1.5/2.75)*t+.75:t<2.5/2.75?7.5625*(t-=2.25/2.75)*t+.9375:7.5625*(t-=2.625/2.75)*t+.984375},InOut:function(t){return t<.5?.5*TWEEN.Easing.Bounce.In(2*t):.5*TWEEN.Easing.Bounce.Out(2*t-1)+.5}}},TWEEN.Interpolation={Linear:function(t,n){var e=t.length-1,i=e*n,r=Math.floor(i),a=TWEEN.Interpolation.Utils.Linear;return n<0?a(t[0],t[1],i):1<n?a(t[e],t[e-1],e-i):a(t[r],t[e<r+1?e:r+1],i-r)},Bezier:function(t,n){for(var e=0,i=t.length-1,r=Math.pow,a=TWEEN.Interpolation.Utils.Bernstein,s=0;s<=i;s++)e+=r(1-n,i-s)*r(n,s)*t[s]*a(i,s);return e},CatmullRom:function(t,n){var e=t.length-1,i=e*n,r=Math.floor(i),a=TWEEN.Interpolation.Utils.CatmullRom;return t[0]===t[e]?(n<0&&(r=Math.floor(i=e*(1+n))),a(t[(r-1+e)%e],t[r],t[(r+1)%e],t[(r+2)%e],i-r)):n<0?t[0]-(a(t[0],t[0],t[1],t[1],-i)-t[0]):1<n?t[e]-(a(t[e],t[e],t[e-1],t[e-1],i-e)-t[e]):a(t[r?r-1:0],t[r],t[e<r+1?e:r+1],t[e<r+2?e:r+2],i-r)},Utils:{Linear:function(t,n,e){return(n-t)*e+t},Bernstein:function(t,n){var e=TWEEN.Interpolation.Utils.Factorial;return e(t)/e(n)/e(t-n)},Factorial:function(){var i=[1];return function(t){var n=1;if(i[t])return i[t];for(var e=t;1<e;e--)n*=e;return i[t]=n}}(),CatmullRom:function(t,n,e,i,r){var a=.5*(e-t),s=.5*(i-n),o=r*r;return(2*n-2*e+a+s)*(r*o)+(-3*n+3*e-2*a-s)*o+a*r+n}}},function(t){"function"==typeof define&&define.amd?define([],function(){return TWEEN}):"undefined"!=typeof module&&"object"==typeof exports?module.exports=TWEEN:void 0!==t&&(t.TWEEN=TWEEN)}(undefined);
+
+
 ;// CONCATENATED MODULE: ./src/script.js
 
 
@@ -6691,6 +6696,32 @@ UnrealBloomPass.BlurDirectionY = new three_module/* Vector2 */.FM8( 0.0, 1.0 );
 
 
 
+
+
+
+function vertexShader () {
+  return `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }
+  `
+}
+
+function fragmentShader () {
+  return `
+  uniform sampler2D baseTexture;
+  uniform sampler2D bloomTexture;
+
+  varying vec2 vUv;
+
+  void main() {
+    gl_FragColor = (texture2D(baseTexture, vUv) + vec4(1.0) * texture2D(bloomTexture, vUv));
+  }
+  `
+}
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -6703,11 +6734,18 @@ const params = {
   bloomRadius: 1
 }
 
+const GLOW_LYR = 1
+const bloomLayer = new three_module/* Layers */.S9g()
+bloomLayer.set(GLOW_LYR)
+const darkMaterial = new three_module/* MeshBasicMaterial */.vBJ({ color: 'black' })
+const tmpMaterials = {}
+
 let camera
-let composer, renderer, mixer, clock
+let bloomComposer, finalComposer, renderer, mixer, clock
 let controls
 let model
 let mesh
+let pageNow = 0
 
 init()
 
@@ -6744,9 +6782,28 @@ function init () {
   bloomPass.strength = params.bloomStrength
   bloomPass.radius = params.bloomRadius
 
-  composer = new EffectComposer(renderer)
-  composer.addPass(renderScene)
-  composer.addPass(bloomPass)
+  bloomComposer = new EffectComposer(renderer)
+  bloomComposer.renderToScreen = false
+  bloomComposer.addPass(renderScene)
+  bloomComposer.addPass(bloomPass)
+
+  const finalPass = new ShaderPass(
+    new three_module/* ShaderMaterial */.jyz({
+      uniforms: {
+        baseTexture: { value: null },
+        bloomTexture: { value: bloomComposer.renderTarget2.texture }
+      },
+      vertexShader: vertexShader(),
+      fragmentShader: fragmentShader(),
+      defines: {}
+    }),
+    'baseTexture'
+  )
+  finalPass.needsSwap = true
+
+  finalComposer = new EffectComposer(renderer)
+  finalComposer.addPass(renderScene)
+  finalComposer.addPass(finalPass)
 
   controls = new OrbitControls(camera, renderer.domElement)
   controls.maxPolarAngle = Math.PI * 0.5
@@ -6757,24 +6814,55 @@ function init () {
   pointLight.position.set(0.5, -1, 3)
   camera.add(pointLight)
 
-  new GLTFLoader().load('./tetrapod.glb', function (gltf) {
-    /*const*/ model = gltf.scene
-    // model.scale.set(0.01, 0.01, 0.01)
-    // model.position.set( - 5, -30.5, - 3.5 );
-    scene.add(model)
-    // mixer = new THREE.AnimationMixer(model)
-    // const clip = gltf.animations[0]
-    // mixer.clipAction( clip.optimize() ).play();
-    // animate()
-  })
+  const manager = new three_module/* LoadingManager */.lLk(/*loadModel*/)
+
+  // Not so practical:
+  // manager.onProgress = (item, loaded, total) => {
+  //   console.log(item, loaded, total)  }
+
+  manager.onLoad = (item, loaded, total) => {
+    console.log('onLoad ' + item, loaded, total)
+  }
+
+  // Not practical for large GLTF:
+  // function onProgress (xhr) {    // not work for large GLTF
+  //   if (xhr.lengthComputable) {  // never triggered for large GLTF
+  //     const percentComplete = (xhr.loaded / xhr.total) * 100
+  //     console.log(Math.round(percentComplete, 2) + '% done')  }  }
+  // function onLoad () {}  // triggered on start
+
+  const loader = new GLTFLoader(manager)
+  loader.load(
+    './tetrapod.glb',
+    gltf => {
+      model = gltf.scene
+      // model.scale.set(0.01, 0.01, 0.01)
+      scene.add(model)
+      model.layers.enable(GLOW_LYR)
+    }
+    // onProgress,  // not work for large GLTF
+    // onLoad       // triggered on start
+  )
+
+  // // new GLTFLoader().load('./tetrapod.glb', function (gltf) {
+  // new GLTFLoader().load('./Bangkok.gltf', function (gltf) {
+  //   /*const*/ model = gltf.scene
+  //   model.scale.set(0.01, 0.01, 0.01)
+  //   // model.position.set( - 5, -30.5, - 3.5 );
+  //   scene.add(model)
+  //   // mixer = new THREE.AnimationMixer(model)
+  //   // const clip = gltf.animations[0]
+  //   // mixer.clipAction( clip.optimize() ).play();
+  //   // animate()
+  // })
 
   const geometry = new three_module/* PlaneBufferGeometry */.BKK()
   const material = new three_module/* MeshBasicMaterial */.vBJ({ side: three_module/* DoubleSide */.ehD })
   mesh = new three_module/* Mesh */.Kj0(geometry, material)
 
   new three_module/* TextureLoader */.dpR().load(
-    './rt4_coming.png',
-    function (image) {
+    './rt4_coming_w.png',
+    image => {
       // image.encoding = THREE.sRGBEncoding;
       material.map = image
       // material.needsUpdate = true;
@@ -6786,7 +6874,7 @@ function init () {
       scene.add(mesh)
     },
     undefined,
-    function (error) {
+    error => {
       console.error(error)
     }
   )
@@ -6796,9 +6884,110 @@ function init () {
   // const cube = new THREE.Mesh(geometry, material)
   // scene.add(cube)
 
+  window.addEventListener('load', onLoad)
   window.addEventListener('resize', onWindowResize)
+  window.addEventListener('pointerup', onPointerUp)
 
   animate()
+}
+
+function pageWarRoom () {
+  if (1 == pageNow) {
+    return
+  }
+  // console.info('pageWarRoom')
+  pageNow = 1
+  const manager = new three_module/* LoadingManager */.lLk(/*loadModel*/)
+
+  // Not so practical:
+  // manager.onProgress = (item, loaded, total) => {
+  //   console.log(item, loaded, total)  }
+
+  manager.onLoad = (item, loaded, total) => {
+    mesh.visible = false
+
+    // const dst = new THREE.Vector3(0.4, 0.4, 0.4)
+    var current = model.scale
+    var tweenHead = new TWEEN.Tween(current)
+      // .to({ x: +800 }, 2500)
+      // .to(controls.target.sub(direction), 1500)
+      // .to(dst, 1500)
+      .to({ x: 0.5, y: 0.5, z: 0.5 }, 1500)
+      // .delay(200)
+      // .easing(TWEEN.Easing['Elastic']['EaseInOut'])
+      .easing(TWEEN.Easing.Quartic.In)
+      .onUpdate(() => {
+        model.scale.copy(current)
+        model.position.y = model.position.y - 0.015
+      })
+    // .onComplete(function () {
+    //   console.info('\nTWEEN.onComplete\n')
+    //   // camera.position.copy(controls.target).sub(direction)
+    //   controls.update()
+    // })
+
+    tweenHead.start()
+  }
+
+  const loader = new GLTFLoader(manager)
+  loader.load(
+    './VirtualWarRoom.glb',
+    gltf => {
+      const warRoomModel = gltf.scene
+      // warRoomModel.scale.set(0.01, 0.01, 0.01)
+      warRoomModel.scale.set(0.5, 0.5, 0.5)
+      warRoomModel.position.set(0, -1.5, 0)
+      scene.add(warRoomModel)
+    }
+    // onProgress,  // not work for large GLTF
+    // onLoad       // triggered on start
+  )
+}
+
+function darkenNonBloomed (obj) {
+  if ('Tetrapod_real_logo_mat' == obj.name) {
+    return
+  }
+  if ('RoundTable_Glow' == obj.name) {
+    return
+  }
+  if ('TVStandGLow' == obj.name) {
+    return
+  }
+  if ('RoundLamp' == obj.name) {
+    return
+  }
+  if (obj.isMesh && bloomLayer.test(obj.layers) === false) {
+    tmpMaterials[obj.uuid] = obj.material
+    obj.material = darkMaterial
+  }
+}
+
+function restoreMaterial (obj) {
+  if (tmpMaterials[obj.uuid]) {
+    obj.material = tmpMaterials[obj.uuid]
+    delete tmpMaterials[obj.uuid]
+  }
+}
+
+function onPointerUp (event) {
+  const raycaster = new three_module/* Raycaster */.iMs()
+  const mouse = new three_module/* Vector2 */.FM8()
+
+  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1
+  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObject(model)
+  if (intersects.length > 0) {
+    pageWarRoom()
+  }
+}
+
+function onLoad () {
+  // const ld_ico = document.querySelector('#loading')
+  // if (ld_ico) {
+  //   ld_ico.style.display = 'none'
+  // }
 }
 
 function onWindowResize () {
@@ -6809,7 +6998,8 @@ function onWindowResize () {
   camera.updateProjectionMatrix()
 
   renderer.setSize(width, height)
-  composer.setSize(width, height)
+  bloomComposer.setSize(width, height)
+  finalComposer.setSize(width, height)
 }
 
 function animate () {
@@ -6817,11 +7007,15 @@ function animate () {
   if (model) {
     model.rotation.y += 0.001
   }
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
+
+  TWEEN.update()
 
   // renderer.render(scene, camera)
-  composer.render()
+  scene.traverse(darkenNonBloomed)
+  bloomComposer.render()
+  scene.traverse(restoreMaterial)
+
+  finalComposer.render()
 }
 
 
@@ -6830,6 +7024,6 @@ function animate () {
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__(226));
+/******/ var __webpack_exports__ = (__webpack_exec__(629));
 /******/ }
 ]);
